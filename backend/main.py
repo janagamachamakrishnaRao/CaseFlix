a773ff1b622b954fc072a7611227cf485911a9a2
YourName. <yourgmail@gmail.com>
2026-05-14 09:12:49 +0530

    fixed upload issue

--- backend/main.py
+++ backend/main.py
@@ -132,26 +132,6 @@ async def upload_file(file: UploadFile = File(...)):
     with open(file_path, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
 
-    # DOCX Conversion
-    if file.filename.endswith(".docx"):
-
-        subprocess.run([
-            r"C:\Program Files\LibreOffice\program\soffice.exe",
-            "--headless",
-            "--convert-to",
-            "pdf",
-            file_path,
-            "--outdir",
-            UPLOAD_FOLDER
-        ])
-
-        pdf_filename = file.filename.replace(".docx", ".pdf")
-
-        file_path = os.path.join(UPLOAD_FOLDER, pdf_filename)
-
-        filename = pdf_filename
-
-    else:
         filename = file.filename
 
     # AI PROCESSING
