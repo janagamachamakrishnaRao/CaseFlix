06b7358bf25c5dc8bfc70acfad04e7cf6e5d032b
YourName. <yourgmail@gmail.com>
2026-05-20 14:44:28 +0530

    sqlite azure fix

--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./caseflix.db"
+DATABASE_URL = "sqlite:////tmp/caseflix.db"
 
 engine = create_engine(
     DATABASE_URL,
