893da9f541b8a5654194fa165371c9e5ca236756
YourName. <yourgmail@gmail.com>
2026-05-20 15:50:12 +0530

    fixed models

--- backend/models.py
+++ backend/models.py
@@ -1,15 +1,24 @@
-from sqlalchemy import Column, Integer, String, Text
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+    Text
+)
+
 from database import Base
+
 
 class Incident(Base):
 
     __tablename__ = "incidents"
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(
+        Integer,
+        primary_key=True,
+        index=True
+    )
 
     filename = Column(String)
-
-    severity = Column(String)
 
     incident_type = Column(String)
 
@@ -17,18 +26,27 @@ class Incident(Base):
 
     location = Column(String)
 
-    risk_score = Column(Integer)
-
     summary = Column(Text)
+
 
 class User(Base):
 
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(
+        Integer,
+        primary_key=True,
+        index=True
+    )
 
-    username = Column(String, unique=True)
+    username = Column(
+        String,
+        unique=True
+    )
 
     password = Column(String)
 
-    role = Column(String, default="employee")
+    role = Column(
+        String,
+        default="employee"
+    )
